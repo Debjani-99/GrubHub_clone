ea957fe5b88158729ec598c1b14dcb0f7f85935a
Your Name <deb96pal@gmail.com>
2025-01-13 01:17:51 -0500

    html and css files

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main_page():
+    return render_template("index.html")
+
+# @app.route("/menu")
+# def render_menu():
+#     return "<ul> <li> Coffee </li> <li> Tea </li> </ul>"
 
 if __name__ == "__main__":
     app.run()
+
